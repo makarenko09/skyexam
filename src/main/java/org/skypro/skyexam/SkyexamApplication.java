d0779810c1dc1ac07bd98f4c4ad2a2d48c6abede
makarenko009 <konmakarenko@outlook.com>
2025-04-02 14:28:24 +0300

    Initial commit

---
+++ src/main/java/org/skypro/skyexam/SkyexamApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.skyexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/skyexam/SkyexamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.skyexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
